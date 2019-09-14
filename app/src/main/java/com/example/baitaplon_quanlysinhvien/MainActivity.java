f20be6e90491372279e96a0fb7fd28b97ee35ead
sieudanlon <hieunguyen312123@gmail.com>
2019-09-14 22:23:26 +0700

    da pusht

--- app/src/main/java/com/example/baitaplon_quanlysinhvien/MainActivity.java
+++ app/src/main/java/com/example/baitaplon_quanlysinhvien/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO
+        //TODO nhìn chỗ này k, a đã push //TODO lên khi chú pull vvef thỳ n đã có
     }
 }
